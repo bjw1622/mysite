0a1a93995eee5483c1c8094fabd9acd355616d41
백재원 <bjw1622@gmail.com>
2023-09-19 12:17:17 +0900

    feat : aop

--- mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
+++ mysite03/src/main/java/com/poscodx/mysite/repository/GuestbookRepository.java
@@ -7,6 +7,7 @@ import java.util.Map;
 import org.apache.ibatis.session.SqlSession;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
+import org.springframework.util.StopWatch;
 
 import com.poscodx.mysite.vo.GuestbookVo;
 
@@ -30,6 +31,13 @@ public class GuestbookRepository {
 	}
 
 	public List<GuestbookVo> findAll() {
-		return sqlSession.selectList("guestbook.findAll");
+		StopWatch sw = new StopWatch();
+		sw.start();
+		List<GuestbookVo> list = sqlSession.selectList("guestbook.findAll");
+		sw.stop();
+		long totalTime = sw.getTotalTimeMillis();
+		System.out.println("——>" + totalTime);
+
+		return list;
 	}
 }
